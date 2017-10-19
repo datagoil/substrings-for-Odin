234ec04d5dd195f2ade717a235fdf3b38dcfc506
Sara S <ida.azalia@protonmail.com>
2017-10-19 17:11:24 -0500

    program works but needs to add how many times each string was found

--- substrings.rb
+++ substrings.rb
@@ -1,19 +1,38 @@
 def substrings(str, arr)
+	matches_array = []	
 	string_array = str.split(/\W|\_/)
 	p string_array
 	p arr
 	string_array & arr
+	string_array.each do |word|
+		arr.each do |dictionary_word|
+			puts "#{word}: #{dictionary_word}" 
+				matches_array.push(dictionary_word) if (word.include?(dictionary_word))
+		end
+	end 
+p matches_array
 end
 
+
+=begin  THIS WORKS BUT NEED TO ADD HOW MANY TIMES IT WAS FOUND
+def substrings(str, arr)
+	matches_array = []	
+	string_array = str.split(/\W|\_/)
+	string_array.each do |word|
+		arr.each do |dictionary_word|
+			matches_array.push(dictionary_word) if (word.include?(dictionary_word))
+		end
+	end 
+p matches_array
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 bugs = ["this", "that", "mine", "cow", "hold"]
 substrings("this_ is it's a cow threshhold", bugs)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
 
 
 
-
-
-=begin
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
     > substrings("below", dictionary)
     => {"below"=>1, "low"=>1}
