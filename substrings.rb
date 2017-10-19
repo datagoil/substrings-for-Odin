ceca5a814d6eea16db027c44fc3d51c64fc723f1
Sara S <ida.azalia@protonmail.com>
2017-10-19 17:35:17 -0500

    finished

--- substrings.rb
+++ substrings.rb
@@ -1,15 +1,15 @@
 def substrings(str, arr)
-	matches_hash = Hash.new()
-	string_array = str.downcase.split(/\W|\_/)
-	string_array.each do |word|
-		arr.each do |dictionary_word|
-			matches_hash[dictionary_word] = 1 if (word.include?(dictionary_word))
+	#hash to hold matching strings. Values initialized to zero. Strings are keys.	
+	matches_hash = Hash.new(0)			
+	string_array = str.downcase.split(/\W|\_/)	#turn string into array. all lowercase
+	string_array.each do |word|			#for each word in the new array
+		arr.each do |dictionary_word|		#compare it to each word in user provied array
+			#if there is a match, make it a key in hash and increase the value by 1			
+			matches_hash[dictionary_word] += 1 if (word.include?(dictionary_word))
 		end
 	end 
-p matches_hash
+p matches_hash						#display matches hash to user
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-bugs = ["this", "that", "mine", "cow", "hold"]
-substrings("this_ is it's a cow threshhold", bugs)
 substrings("Howdy partner, sit down! How's it going?", dictionary)
